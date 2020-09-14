f1036561f37214aa443ed7976e8c8b55590fad2c
Curtis <chender93@gmail.com>
2020-09-13 20:20:02 -0400

    Created function to pass the previous searched cities through the function

--- js/script.js
+++ js/script.js
@@ -3,19 +3,17 @@ mainCityEl = document.querySelector("#featured-city");
 foreHeader = document.querySelector("#foreText");
 forecastEl = document.querySelector("#upcoming");
 searchBtn = document.querySelector("#button-addon2");
+loggedCities = JSON.parse(localStorage.getItem("cityList")) || [];
+cityBtn = document.querySelector(".searchedCities");
 todaysDate = (moment().format("L"));
 dayCounter = 1;
+
+
 
 searchBtn.onclick = function (choice) {
     //Logs the user's search choice
     choice = document.querySelector("#searchedCity").value;
-
-    //Creates a button that will take users to previously searched cities
-    searchedCity = document.createElement("button");
-    searchedCity.className = "list-group-item";
-    searchedCity.textContent = choice;
-    cityListEl.appendChild(searchedCity);
-
+    makeList(choice);
     //Fetch Request colleting weather information
     if (!choice) {
         alert("You must select a city before searching!");
@@ -69,14 +67,24 @@ searchBtn.onclick = function (choice) {
                         fList.appendChild(uvIco);
                     })
             })
-        fiveDay();
+        fiveDay(choice);
     }
-    //Append previous searches to the left-hand menu.
 
 };
 
+makeList = function () {
+    //Creates a button from searched cities
+    choice = document.querySelector("#searchedCity").value;
+    searchedCity = document.createElement("button");
+    searchedCity.className = "list-group-item";
+    searchedCity.setAttribute("recentCity", choice);
+    searchedCity.textContent = choice;
+    cityListEl.appendChild(searchedCity);
+};
+
+
 //Create a for loop that will display cooresponding information for the upcoming 5 days
-fiveDay = function (choice) {
+fiveDay = function () {
     choice = document.querySelector("#searchedCity").value;
     fetch("http://api.openweathermap.org/data/2.5/forecast?q="
         + choice +
@@ -100,5 +108,59 @@ fiveDay = function (choice) {
                 dayCounter++
             }
         })
-}
+};
+
+//Create a function that will pass the previously seached cities to search term
+cityBtn.onclick = function (recent) {
+    recalledCity = recent.target.getAttribute("recentCity")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + recalledCity +
+            "&appid=f19269e626e5152019a125e36257aaf9&units=imperial")
+            .then(function (cityInfo) {
+                return cityInfo.json();
+            })
+            .then(function (cityInfo) {
+                //Clear out previous search result
+                mainCityEl.innerHTML = "";
+                forecastEl.innerHTML = "";
+                //Create Weather Icon
+                weatherIco = document.createElement("div");
+                weatherIco.innerHTML = "<img src='http://openweathermap.org/img/w/" + cityInfo.weather[0].icon + ".png' />";
+                //Create searched city result
+                featureEl = document.createElement("h2");
+                featureEl.textContent = cityInfo.name + " (" + todaysDate + ") ";
+                fList = document.createElement("p");
+                fList.setAttribute('style', 'white-space: pre;');
+                fList.textContent = "Temperature: " + cityInfo.main.temp + "\r\n";
+                fList.textContent += "Humidity: " + cityInfo.main.humidity + "%\r\n";
+                fList.textContent += "Wind Speed: " + cityInfo.wind.speed + "\r\n";
+                //Appending Everything to the Main Display
+                featureEl.appendChild(weatherIco);
+                mainCityEl.appendChild(featureEl);
+                mainCityEl.appendChild(fList);
+                fetch("http://api.openweathermap.org/data/2.5/uvi?appid=f19269e626e5152019a125e36257aaf9&lat="
+                    //Insert lat coordinates from above search
+                    + cityInfo.coord.lat +
+                    "&lon="
+                    //Insert lon coordinated from above search
+                    + cityInfo.coord.lat
+                )
+                    .then(function (uvInfo) {
+                        return uvInfo.json();
+                    })
+                    .then(function (uvInfo) {
+                        uvIco = document.createElement("div");
+                        if (uvInfo.value >= 8) {
+                            uvIco.innerHTML = "<img src='https://raw.githubusercontent.com/chender93/weather-dashboard/feature/city-search/images/badUV.png' />"
+                        } else if (uvInfo.value >= 6) {
+                            uvIco.innerHTML = "<img src='https://raw.githubusercontent.com/chender93/weather-dashboard/feature/city-search/images/neutraUV.png' />"
+                        } else {
+                            uvIco.innerHTML = "<img src='https://raw.githubusercontent.com/chender93/weather-dashboard/feature/city-search/images/goodUV.png' />"
+                        }
+                        fList.textContent += "UV Index: " + uvInfo.value;
+                        fList.appendChild(uvIco);
+                    })
+            })
+        fiveDay(recalledCity);
+};
 
